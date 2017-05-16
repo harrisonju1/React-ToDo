811c6a1987d1d5f3ccd70d84c425a7eb75ac1b77
harrisonju1 <harrisonju@gmail.com>
2017-05-16 00:32:00 -0400

    wipe todos on logout

--- app/reducers/reducers.jsx
+++ app/reducers/reducers.jsx
@@ -37,6 +37,8 @@ export var todosReducer = (state = [], action) => {
           return todo;
         }
       });
+    case 'LOGOUT':
+      return [];
     case 'ADD_TODOS':
       return [
         ...state,
@@ -51,7 +53,7 @@ export var authReducer = (state = {}, action) => {
   switch (action.type){
     case 'LOGIN':
       return {
-        uid: action.uid        
+        uid: action.uid
       };
     case 'LOGOUT':
       return {};
